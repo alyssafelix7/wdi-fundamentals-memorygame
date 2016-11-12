c5fb06957de60c8af08e87137c2499d8f38fd4b8
alyssafelix7 <alyssa.felix7@gmail.com>
2016-11-12 11:05:45 -0500

    Created cards for game-board using javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,7 +4,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
   alert ("You found a match!");
 }
 else {
@@ -31,3 +31,22 @@ if (cardThree === cardFour) {
 else {
   alert("Sorry, try again!");
 }
+*/
+
+// Gets element game-board and sets it to var gameBoard
+var gameBoard = document.getElementById('game-board');
+
+// Creates var for number of cards
+var numOfCards = 4;
+
+// Function that creates card and appends them to game-board
+var createCards = function(numOfCards){
+  for (var i = 0; i < numOfCards; i++ ) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    gameBoard.appendChild(newCard);
+  }
+};
+
+// Calls functions
+createCards(numOfCards);
